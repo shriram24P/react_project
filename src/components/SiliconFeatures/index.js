0deca2c03acdb8f008aac5b7af114e90d2ab0822
Shriram Patil <patil.shriram98@gmail.com>
2022-12-10 00:43:40 +0530

    Adding details

--- src/components/SiliconFeatures/index.js
+++ src/components/SiliconFeatures/index.js
@@ -1,40 +1,26 @@
-
+import records from "./records";
 import "./styles.css";
-import React from "react";
-
+import React, { useState } from "react";
 
 const SiliconFeatures = () => {
+  const [data, setData] = useState(records);
   return (
     <>
       <div className="flex-container">
-        <div className="main-box">
-          <img src="./images/1.svg" alt="1" />
-          <br />
-          <br />
-          <h5 className="head-m">Light/Dark mode</h5>
-        </div>
-        <div className="main-box">
-          <img src="./images/2.svg" alt="2" />
-          <br />
-          <br />
-          <h5 className="head-m">Figma Files Included</h5>
-        </div>
-        <div className="main-box">
-          <img src="./images/3.svg" alt="3" />
-          <br />
-          <br />
-          <h5 className="head-m">100+ UI Flexible Section</h5>
-        </div>
-        <div className="main-box">
-          <img src="./images/4.svg" alt="4" />
-          <br />
-          <br />
-          <h5 className="head-m">Free Lifetime Updates</h5>
-        </div>
+        {data.map((items) => {
+          const { id, ImageUrl, title } = items;
+          return (
+            <div className="main-box" key={id}>
+              <img src={ImageUrl} alt="" />
+              <br />
+              <br />
+              <h5 className="head-m">{title}</h5>
+            </div>
+          );
+        })}
       </div>
     </>
   );
 };
 
 export default SiliconFeatures;
-
---
+++ src/components/SiliconFeatures/records.js
@@ -0,0 +1,24 @@
+const records = [
+    {
+      id: 1,
+      ImageUrl: "./images/1.svg",
+      title: "Light/Dark mode",
+    },
+    {
+        id: 2,
+        ImageUrl: "./images/2.svg",
+        title: "Figma Files Included",
+    },
+    {
+        id: 3,
+        ImageUrl: "./images/3.svg",
+        title: "100+ UI Flexible Section",
+    },
+    {
+        id: 4,
+        ImageUrl: "./images/4.svg",
+        title: "Free Lifetime Updates",
+    }
+]
+
+export default records;
